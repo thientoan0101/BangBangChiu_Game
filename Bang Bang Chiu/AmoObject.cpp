16d48cf8f49ad6e59dc0f347a86491c7d016e1a7
Dong <agendacity0318032430@gmail.com>
2020-08-20 22:06:34 +0700

    Present & Amo
    Đã thêm các file Present.h, Present.cpp, AmoObject.h, AmoObject.cpp
    Ngày  20/08/2020

---
+++ Bang Bang Chiu/AmoObject.cpp
@@ -0,0 +1,33 @@
+﻿#include "AmoObject.h"
+
+AmoObject::AmoObject() {
+	y_val_ = 0;
+	x_val_ = 5;																// Tốc độ di chuyển theo chiều ngang mặc định là 5
+	rect_.x = rect_.y = 0;
+	is_move_ = false;
+	amo_type_ = AmoType::NONE;
+}
+
+AmoObject::~AmoObject() {
+	// Do something here
+}
+
+void AmoObject::handleMove(const int& x_border, const int& y_border) {
+	if (is_move_) {
+		if (rect_.x > x_border + WIDTH_BULLET)
+			is_move_ = false;
+		rect_.x += x_val_;
+	}
+}
+
+void AmoObject::handleInputAction(SDL_Event events) {
+	// Do something here
+}
+
+void AmoObject::handleMoveRightToLeft() {
+	if (is_move_) {
+		if (rect_.x < -WIDTH_BULLET)
+			is_move_ = false;
+		rect_.x -= x_val_;
+	}
+}
---
+++ Bang Bang Chiu/Present.cpp
@@ -0,0 +1,24 @@
+﻿#include "Present.h"
+
+Present::Present() {
+	rect_.x = rect_.y = 0;												// Tọa độ xuất hiện mặc định là gốc tọa độ
+	x_val_ = y_val_ = 0;												// Tốc độ di chuyển mặc định là 0 theo cả chiều dọc va chiều ngang
+	type_ = Present::POWER;												// Mặc định là có kiểu POWER
+	is_move_ = true;													// Mặc định được phép di chuyển
+}
+
+Present::~Present() {
+	// Do something here
+}
+
+void Present::handleInputAction(SDL_Event events) {
+	// Do something here
+}
+
+void Present::handleMove(const int& x_border, const int& y_border) {
+	if (is_move_) {
+		rect_.x -= x_val_;
+		if (rect_.x < -WIDTH_POWER)
+			is_move_ = false;
+	}
+}
